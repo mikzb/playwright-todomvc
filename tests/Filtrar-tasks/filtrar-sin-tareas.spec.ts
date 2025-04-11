88ce1bc0507310a2f6d50d4571900fc55d0b9407
AdamAhmedMohamed <mohamedahmedadam08@gmail.com>
2025-04-11 13:09:31 +0200

    Fix: handle hidden footer in 'filtrar-sin-tareas' test

--- tests/Filtrar-tasks/filtrar-sin-tareas.spec.ts
+++ tests/Filtrar-tasks/filtrar-sin-tareas.spec.ts
@@ -3,6 +3,11 @@ import { test, expect } from '@playwright/test';
 test('Aplicar filtros sin tareas', async ({ page }) => {
   await page.goto('https://todomvc.com/examples/react/dist/#/');
 
-  await page.locator('text=Active').click();
+  // Clic en filtro "Active"
+  await page.locator('a:has-text("Active")').first().click();
+  await expect(page.locator('.todo-list li')).toHaveCount(0);
+
+  // Clic en filtro "Completed"
+  await page.locator('a:has-text("Completed")').first().click();
   await expect(page.locator('.todo-list li')).toHaveCount(0);
 });
